284d7b3a303a0954c03522fd68b6e6af1ccb1ca6
Rober <devsobarzo@gmail.com>
2024-09-19 21:50:53 +0700

    Character's lower movement

--- Assets/Scripts/CharMovement.cs
+++ Assets/Scripts/CharMovement.cs
@@ -18,9 +18,17 @@ namespace BASA
 
         Vector3 speedFall;
 
+        public Transform cameraTransform;
+        public bool isBend;
+        public bool upBlocked;
+        public float heightUp, heightDown, posCamUp, posCamDown;
+        RaycastHit hit;
+
         void Start()
         {
             controller = GetComponent<CharacterController>();
+            isBend = false;
+            cameraTransform = Camera.main.transform;
         }
 
 
@@ -47,6 +55,50 @@ namespace BASA
 
             speedFall.y += gravity * Time.deltaTime;
             controller.Move(speedFall * Time.deltaTime);
+
+            if (isBend)
+            {
+                CheckBlockedDown();
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                Bend();
+            }
+        }
+
+        void Bend()
+        {
+            if (upBlocked || isOnGround == false)
+            {
+                return;
+            }
+
+            isBend = !isBend;
+            if (isBend)
+            {
+                controller.height = heightDown;
+                cameraTransform.localPosition = new Vector3(0, posCamDown, 0);
+            }
+            else
+            {
+                controller.height = heightUp;
+                cameraTransform.localPosition = new Vector3(0, posCamUp, 0);
+            }
+        }
+
+        void CheckBlockedDown()
+        {
+            Debug.DrawRay(cameraTransform.position, Vector3.up * 1.1f, Color.red);
+
+            if (Physics.Raycast(cameraTransform.position, Vector3.up, out hit, 1.1f))
+            {
+                upBlocked = true;
+            }
+            else
+            {
+                upBlocked = false;
+            }
         }
 
         void OnDrawGizmosSelected()
