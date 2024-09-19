909b8580629e5ff93e83d51251d6e6a58cbef75b
Rober <devsobarzo@gmail.com>
2024-09-19 19:27:56 +0700

    Character's movement added

---
+++ Assets/Scripts/CharMovement.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BASA
+{
+    public class CharMovement : MonoBehaviour
+    {
+        public CharacterController controller;
+        public float speed = 6f;
+        public float heightJump = 3f;
+        public float gravity = -20f;
+
+        public Transform checkGround;
+        public float radiousSphere = 0.4f;
+        public LayerMask groundMask;
+        public bool isOnGround;
+
+        Vector3 speedFall;
+
+        void Start()
+        {
+            controller = GetComponent<CharacterController>();
+        }
+
+
+        void Update()
+        {
+            isOnGround = Physics.CheckSphere(checkGround.position, radiousSphere, groundMask);
+
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = (transform.right * x + transform.forward * z).normalized;
+
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
+        void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(checkGround.position, radiousSphere);
+        }
+    }
+}
